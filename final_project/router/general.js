445fefbb4721dcaeb278f1aa266a4227df1bd2a6
Zoltan Bardossi <sunnystudiohu@gmail.com>
2024-07-13 11:58:37 +0200

    Task 3 - get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,7 +38,19 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  let bookByAuthor = [];
+
+  for (key in books) {
+    if (books[key].author === author) {
+      bookByAuthor.push(books[key]);
+    }
+  }
+  if (bookByAuthor.length > 0) {
+    return res.status(200).json(bookByAuthor);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get all books based on title
