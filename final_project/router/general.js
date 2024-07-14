0f94645aae371cb0d8c603048d4fe0123ba06a15
Zoltan Bardossi <sunnystudiohu@gmail.com>
2024-07-14 10:44:27 +0200

    Task 5 completed - get book review by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -74,7 +74,20 @@ public_users.get('/title/:title', function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  let reviewByIsbn = null;
+
+  for (key in books) {
+    if (books[key].isbn === isbn) {
+      reviewByIsbn = books[key]["reviews"];
+    }
+  }
+  if (reviewByIsbn) {
+    return res.status(200).json(reviewByIsbn);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
 });
 
 module.exports.general = public_users;
